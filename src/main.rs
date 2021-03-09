18ae2f88903a888d18e5080cc782cd38e0b2dd51
Hannan Azmi <mhannan143@gmail.com>
2021-03-10 00:28:01 +0800

    Print arguments received from cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let mut args: Vec<String> = Vec::new();
+    handle_arguments(&mut args);
+    println!("{:?}", args);
+}
+
+fn handle_arguments(args: &mut Vec<String>) {
+    for arg in std::env::args().skip(1) {
+        println!("{}", arg);
+        args.push(arg);
+    }
 }
